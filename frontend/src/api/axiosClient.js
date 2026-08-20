990a4d63900ceff740c5cd5eea8a0e1e1374fc21
kritikabharti <kritikabharti577@gmail.com>
2026-08-20 11:18:05 +0530

    Vprotech Task Management ClientUrl

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -3,8 +3,10 @@ import { store } from '../store/store';
 import { logout } from '../store/authSlice';
 
 const axiosClient = axios.create({
-  baseURL: '/api',
-  headers: { 'Content-Type': 'application/json' },
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 axiosClient.interceptors.request.use((config) => {
